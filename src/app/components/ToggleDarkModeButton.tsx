75b2dfbcde376679f54c88b48690d7bd7644a4cc
granth2023 <grantharris38@gmail.com>
2024-02-20 10:28:55 -0500

    export default toggle dark mode button

--- src/app/components/ToggleDarkModeButton.tsx
+++ src/app/components/ToggleDarkModeButton.tsx
@@ -1,2 +1,15 @@
 import React from 'react';
 import { useTheme } from '../../Context/ThemeContext'
+
+const ToggleDarkModeButton = () => { 
+    const { darkMode, toggleDarkMode } = useTheme(); 
+
+
+    return (
+        <button onClick={toggleDarkMode}> 
+            Switch to {darkMode ? 'Light' : 'Dark'} Mode
+        </button>
+    )
+}
+
+export default ToggleDarkModeButton;
